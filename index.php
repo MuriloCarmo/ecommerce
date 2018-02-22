f814f273b073180490b2d0b0939af87de63b2700
Murilo henrique do Carmo <murilo.carmo@seduc.go.gov.br>
2018-02-22 11:43:54 -0300

    Criando template das páginas

--- index.php
+++ index.php
@@ -1,17 +1,30 @@
-<?php 
+<?php
 
-require_once("vendor/autoload.php");
+require_once( "vendor/autoload.php" );
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Hcode\Page;
+use \Hcode\PageAdmin;
 
-$app->config('debug', true);
+$app = new Slim();
 
-$app->get('/', function() {
-    
-	echo "OK";
+$app->config( 'debug', true );
 
-});
+$app->get( '/', function () {
+
+	$page = new Page();
+
+	$page->setTpl( "index" );
+
+} );
+
+$app->get( '/admin', function () {
+
+	$page = new PageAdmin();
+
+	$page->setTpl( "index" );
+
+
+} );
 
 $app->run();
-
- ?>
